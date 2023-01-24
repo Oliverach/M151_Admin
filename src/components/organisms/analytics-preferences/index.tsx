ec8ddcd94adaa7bd0309f24a76656b98515870bf
oliverach <oliverachermann23@gmail.com>
2023-01-24 10:53:55 +0100

    disabled analytics

--- src/components/organisms/analytics-preferences/index.tsx
+++ src/components/organisms/analytics-preferences/index.tsx
@@ -79,62 +79,64 @@ const AnalyticsPreferencesModal = () => {
   })
 
   return (
-    <FocusModal>
-      <FocusModal.Main>
-        <div className="flex flex-col items-center">
-          <div className="mt-5xlarge flex flex-col max-w-[664px] w-full">
-            <h1 className="inter-xlarge-semibold mb-large">
-              Help us get better
-            </h1>
-            <p className="text-grey-50">
-              To create the most compelling e-commerce experience we would like
-              to gain insights in how you use Medusa. User insights allow us to
-              build a better, more engaging, and more usable products. We only
-              collect data for product improvements. Read what data we gather in
-              our{" "}
-              <a
-                href="https://docs.medusajs.com/usage"
-                rel="noreferrer noopener"
-                target="_blank"
-                className="text-violet-60"
-              >
-                documentation
-              </a>
-              .
-            </p>
-            <div className="mt-xlarge flex flex-col gap-y-xlarge">
-              <InputField
-                label="Email"
-                placeholder="you@company.com"
-                disabled={watchOptOut || watchAnonymize}
-                className={clsx("transition-opacity", {
-                  "opacity-50": watchOptOut || watchAnonymize,
-                })}
-                {...register("email", {
-                  pattern: {
-                    message: "Please enter a valid email",
-                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                  },
-                })}
-                errors={errors}
-              />
-              <AnalyticsConfigForm form={nestedForm(form, "config")} />
-            </div>
-            <div className="flex items-center justify-end mt-5xlarge">
-              <Button
-                variant="primary"
-                size="small"
-                loading={isLoading}
-                onClick={onSubmit}
-              >
-                Continue
-              </Button>
-            </div>
-          </div>
-        </div>
-      </FocusModal.Main>
-    </FocusModal>
+    // <FocusModal>
+    //   <FocusModal.Main>
+    //     <div className="flex flex-col items-center">
+    //       <div className="mt-5xlarge flex flex-col max-w-[664px] w-full">
+    //         <h1 className="inter-xlarge-semibold mb-large">
+    //           Help us get better
+    //         </h1>
+    //         <p className="text-grey-50">
+    //           To create the most compelling e-commerce experience we would like
+    //           to gain insights in how you use Medusa. User insights allow us to
+    //           build a better, more engaging, and more usable products. We only
+    //           collect data for product improvements. Read what data we gather in
+    //           our{" "}
+    //           <a
+    //             href="https://docs.medusajs.com/usage"
+    //             rel="noreferrer noopener"
+    //             target="_blank"
+    //             className="text-violet-60"
+    //           >
+    //             documentation
+    //           </a>
+    //           .
+    //         </p>
+    //         <div className="mt-xlarge flex flex-col gap-y-xlarge">
+    //           <InputField
+    //             label="Email"
+    //             placeholder="you@company.com"
+    //             disabled={watchOptOut || watchAnonymize}
+    //             className={clsx("transition-opacity", {
+    //               "opacity-50": watchOptOut || watchAnonymize,
+    //             })}
+    //             {...register("email", {
+    //               pattern: {
+    //                 message: "Please enter a valid email",
+    //                 value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+    //               },
+    //             })}
+    //             errors={errors}
+    //           />
+    //           <AnalyticsConfigForm form={nestedForm(form, "config")} />
+    //         </div>
+    //         <div className="flex items-center justify-end mt-5xlarge">
+    //           <Button
+    //             variant="primary"
+    //             size="small"
+    //             loading={isLoading}
+    //             onClick={onSubmit}
+    //           >
+    //             Continue
+    //           </Button>
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </FocusModal.Main>
+    // </FocusModal>
+    <></>
   )
 }
 
-export default AnalyticsPreferencesModal
+ export default AnalyticsPreferencesModal
+
